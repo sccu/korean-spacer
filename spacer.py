64e2fe7ab702ac00407ea25e9118731ad5c19724
jujang <jujang@sk.com>
2017-09-07 10:15:08 +0900

    에러 없이 루프를 돌도록 수정한다.

--- spacer.py
+++ spacer.py
@@ -1,4 +1,5 @@
 import torch
+from torch.autograd import Variable
 
 if torch.cuda.is_available():
   import torch.cuda as tc
@@ -7,12 +8,27 @@ else:
 
 
 class Spacer(torch.nn.Module):
-  def __init__(self, num_embeddings, embedding_dim, input_size, hidden_size, num_layers, dropout,
+  def __init__(self, num_embeddings, embedding_dim, hidden_size, num_layers, dropout,
       bidirectional):
-    super()
+    super().__init__()
+    self.num_layers = num_layers
+    self.num_directions = 2 if bidirectional else 1
+    self.hidden_size = hidden_size
     self.embedding = torch.nn.Embedding(num_embeddings, embedding_dim)
-    self.lstm = torch.nn.LSTM(input_size, hidden_size, num_layers, dropout=dropout, bidirectional=bidirectional)
-    self.linear = torch.nn.Linear(hidden_size, num_embeddings)
+    self.lstm = torch.nn.LSTM(embedding_dim, hidden_size, num_layers, dropout=dropout, bidirectional=bidirectional)
+    self.linear = torch.nn.Linear(hidden_size * self.num_directions, 1)
+
+    #num_layers * num_directions, batch, hidden_size
+
+  def init_hiddens(self, batch_size):
+    h0 = Variable(tc.zeros([self.num_layers * self.num_directions, batch_size, self.hidden_size]), requires_grad=False)
+    c0 = Variable(tc.zeros([self.num_layers * self.num_directions, batch_size, self.hidden_size]), requires_grad=False)
+    return h0, c0
 
   def forward(self, input):
-    pass
+    l = self.embedding(input)
+    batch_size = input.size()[0]
+    h0, c0 = self.init_hiddens(batch_size)
+    out, (h_n, c_n) = self.lstm(l, (h0, c0))
+    out = self.linear(out)
+    return out
--- train.py
+++ train.py
@@ -9,12 +9,17 @@ from torchtext import datasets
 
 EMBEDDING_DIM = 16
 PADDING_IDX = 0
+HIDDEN_SIZE = 128
+NUM_LAYER = 3
+DROPOUT = 0.2
+BIDIRECTIONAL = True
+
 
 # num_embeddings, embedding_dim, padding_idx, input_size, hidden_size, num_layers, dropout, bidirectional):
 
 def main():
-  src = data.Field(include_lengths=True, tokenize=list)
-  tgt = data.Field(include_lengths=True, tokenize=list)
+  src = data.Field(include_lengths=True, tokenize=list, batch_first=True)
+  tgt = data.Field(include_lengths=True, tokenize=list, batch_first=True)
 
   mt_train = datasets.TranslationDataset(
     path='data/test', exts=('.src', '.tgt'),
@@ -30,18 +35,20 @@ def main():
 
   print("Making batches..")
   train_iter = data.BucketIterator(
-    dataset=mt_train, batch_size=64,
+    dataset=mt_train, batch_size=1,
     device=-1,
     # sort_key=lambda x: data.interleave_keys(len(x.src), len(x.trg))
     sort_key=lambda x: -len(x.src)
   )
 
   from spacer import Spacer
-  model = Spacer(len(src.vocab), EMBEDDING_DIM, src.vocab.stoi)
+  model = Spacer(len(src.vocab), EMBEDDING_DIM, HIDDEN_SIZE, NUM_LAYER, DROPOUT, BIDIRECTIONAL)
 
   for batch_idx, batch in enumerate(train_iter):
+    out = model(batch.src[0])
+    for example in batch.src[0][0]:
+      print("step: {}, {}".format(example.data[0], src.vocab.itos[example.data[0]]))
     print(batch_idx)
-
 
   print('done')
 
