01cf41aca2a5b4b376aa944f1308f2510f05211c
jujang <jujang@sk.com>
2017-09-14 22:45:05 +0900

    Resolve #5 Optimization experiment: Apply unbuffering according to the OS types.

--- experiments.sh
+++ experiments.sh
@@ -2,7 +2,7 @@
 set -e
 
 DATETIME=$(date "+%Y-%m-%d_%H_%M")
-echo $DATETIME
+PLATFORM=$(uname)
 
 PRJ_DIR=$(dirname $0)
 mkdir -p $PRJ_DIR/var/log
@@ -15,17 +15,24 @@ DROPOUT=(0.1 0.3)
 LR=(1.0 0.5 0.1)
 BATCH_SIZE=64
 
+if [ "$PLATFORM" == "Linux" ]; then
+  CMD="stdbuf -o 0 python3 ./train.py --epochs 10 -b 512"
+elif [ "$PLATFORM" == "Darwin" ]; then
+  CMD="script -q /dev/null python3 ./train.py -b 64 "
+else
+  echo "Unsupported platform: $PLATFORM"
+  exit 1
+fi
+
 for emb in "${EMB_DIM[@]}"; do
   for hidden in "${HIDDEN[@]}"; do
     for nlayer in "${NLAYER[@]}"; do
       for dropout in "${DROPOUT[@]}"; do
-        echo "START: -b 64 --embedding-size $emb --hidden-size $hidden --nlayers $nlayer --dropout $dropout" \
-          | tee -a $LOG
+        echo "START: --embedding-size $emb --hidden-size $hidden --nlayers $nlayer --dropout $dropout" | tee -a $LOG
         date | tee -a $LOG
-        script -q /dev/null ./train.py -b 64 --embedding-size $emb --hidden-size $hidden --nlayers $nlayer \
-          --dropout $dropout | tee -a $LOG 2>&1
+        $CMD --embedding-size $emb --hidden-size $hidden --nlayers $nlayer --dropout $dropout | tee -a $LOG 2>&1
         date | tee -a $LOG
-        echo "END: -b 64 --embedding-size $emb --hidden-size $hidden --nlayers $nlayer --dropout $dropout" | tee -a $LOG
+        echo "END: --embedding-size $emb --hidden-size $hidden --nlayers $nlayer --dropout $dropout" | tee -a $LOG
       done
     done
   done
